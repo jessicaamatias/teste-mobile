8e4183cccef7f40adfeb3d1c7ace9ba6880734d4
jessicaamatias <54812515+jessicaamatias@users.noreply.github.com>
2020-03-01 23:12:38 +0000

    Add files via upload

--- www/js/script.js
+++ www/js/script.js
@@ -1,26 +1,35 @@
+// array to save favourites chosen by user
 var dataSetFavorites = [];
 
 $(document).ready(function() {
 	
-	// set arrays of the news by suject type
+	//calling the functions
 	getDate();
+	//sending each type of news by parameter to the function:
 	getNews('sport');
 	getNews('business');
 	getNews('financial');
 	
 	// add news into array of the favorites
+	//everytime the user clicks on 'add' 
 	$(".add-favorite").on("click", function(e) {
 		var idLine = $(this).attr("id");
+		
 		var newsExists = false;
+		//validating if the news was already added
 		$.each(dataSetFavorites, function() {
 			if(idLine == this.idLine){
 				newsExists = true;
+				
 				return false;
 			}	
 		});	
 		
+		//if news is not already on the array it is then added:
 		if(!newsExists){
+			//published date
 			var publishedAt = $("#publishedAt" + idLine).html();
+			//news description
 			var description = $("#description" + idLine).html();
 			dataSetFavorites.push({idLine, publishedAt, description});
 			setFavorite();			
@@ -31,23 +40,33 @@ $(document).ready(function() {
 
 // get moment date and shows into home page
 function getDate() {
+	
 	$('#dataTable-home').html(new Date());	
 }
 
 // call rest-service from website news-api
-function getNews(type) {	
+//function that pulls the news according to the type sent by parameter
+function getNews(type) {
+	//the url variable receives the api url with each news type sent by parameter	
 	var url = 'http://newsapi.org/v2/everything?q='+type+'&apiKey=d77b40b94e714053b0e20391cad1954b';
+	//variable count so I can use the same fucntion to all different types of news
 	var count = 0;
 	$.ajax({
-		url : url,
+		url : url,		
 		type : "GET",
+		//needs to be 'sync', that means the api is responding to load the rest		
 		async : false, 
-		success : (function(data, status, jqXhr) {
+		success : (function(data, status, jqXhr) {//j query standard
 			$.each(data.articles, function() {
+				//indenfying each news by its own id to be used when printing
 				var id = type + count;
-				var line = "<tr><td id='publishedAt"+id+"'>"+ new Date(this.publishedAt).toLocaleDateString("en-IE") +"</td>" 
+				//selecting data to be shown on a table
+				var line = "<tr><td id='publishedAt"+id+"'>"
+				//making sure it is shown in the correct format to ireland
+				+ new Date(this.publishedAt).toLocaleDateString("en-IE") +"</td>" 
 				+ "<td id='description"+id+"'>"+ this.description +"</td>"
 				+ "<td><a href='#' id='"+id+"' class='add-favorite'>add</a></td></tr>";
+				//teste
 				setDataTable(line, type);
 				count++; 
 			});			
@@ -57,15 +76,19 @@ function getNews(type) {
 
 // parse json to datatable
 function setDataTable(line, type) {
+	
 	$('#tbody-' + type).append(line);	
 }
 
 // parse array to favorite datatable
 function setFavorite() {
+	//id used in the html file
 	$('#tbody-favorites').empty();
 	$.each(dataSetFavorites, function() {
+		//saving the date of publishment and the news description into the var line
 		var line = "<tr><td>"+ this.publishedAt +"</td>" 
 		+ "<td>"+ this.description +"</td>"
+		//sending var line and type favorites as parameter to the function:
 		setDataTable(line, 'favorites');
 	});		
 }
